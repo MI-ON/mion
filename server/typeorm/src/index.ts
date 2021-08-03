94fa79308b36b5bc30436de7d0bfcb80ea495969
nayoon <s2019s05@e-mirim.hs.kr>
2021-08-04 01:56:48 +0900

    Setting typeorm from  localhost

---
+++ server/typeorm/src/index.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "mirim2",
+    database: "test",
+    entities: [
+        User
+    ],
+    synchronize: true,
+    logging: false
+}).then(connection => {
+    console.log("성공");
+    // here you can start to work with your entities
+}).catch(error => console.log(error));
+
+
+
+
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
