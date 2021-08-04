5ee8e62bb381d4d3947e561a7b81cac0ae602878
mia <s2019w27@e-mirim.hs.kr>
2021-08-05 00:28:25 +0900

    Update marker image & marker click event

--- client/src/components/map/map.component.ts
+++ client/src/components/map/map.component.ts
@@ -19,6 +19,8 @@ export default class MapComponent extends Vue {
   place:string | any = "";
   bounds:string | any = "";
   search:string | any = "";
+  imageSrc: string = "";
+  selectedMarker: string | any = null;
 
   mounted() {
     this.openKakakoMap()
@@ -27,7 +29,7 @@ export default class MapComponent extends Vue {
   public openKakakoMap() {
     console.log( "openKakakoMap()")
 
-    // 마{커를 클릭하면 장소명을 표출할 인포윈도우
+    // 마커를 클릭하면 장소명을 표출할 인포윈도우
     this.infowindow = new window.kakao.maps.InfoWindow({zIndex: 1});
 
     // 지도를 표시할 div
@@ -63,7 +65,6 @@ export default class MapComponent extends Vue {
       console.log( 'placesSearchCB() START!!')
 
       if (status === window.kakao.maps.services.Status.OK) {
-
           // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
           // LatLngBounds 객체에 좌표 추가
           this.bounds = new window.kakao.maps.LatLngBounds();
@@ -85,16 +86,50 @@ export default class MapComponent extends Vue {
     console.log( 'displayMarker() START!!' )
     console.log(data)
 
+    const mapMarker = require('../../assets/default-marker.png'),  // default 마커 이미지    
+    imageSize = new window.kakao.maps.Size(64, 69), // 마커이미지의 크기
+    imageOption = {offset: new window.kakao.maps.Point(27, 69)}; // 마커이미지의 옵션 - 마커의 좌표와 일치시킬 이미지 안에서의 좌표 설정
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성
+    const markerImage = new window.kakao.maps.MarkerImage(mapMarker, imageSize, imageOption);
+
+
+    const clickMapMarker = require('../../assets/click-marker.png');  // default 마커 이미지    
+    const clickMarkerImage = new window.kakao.maps.MarkerImage(clickMapMarker, imageSize, imageOption);
+    
     // 마커를 생성하고 지도 표시
     const marker1 = new window.kakao.maps.Marker({
         map: this.map,
-        position: new window.kakao.maps.LatLng(data.y, data.x)
+        position: new window.kakao.maps.LatLng(data.y, data.x),
+        image: markerImage
+    });
+
+
+    const iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+    // 인포윈도우를 생성합니다
+    this.infowindow = new window.kakao.maps.InfoWindow({
+      removable : iwRemoveable
     });
 
     // 마커에 클릭이벤트 등록
     window.kakao.maps.event.addListener(marker1, 'click', () => {
         console.log( 'clickMarker() START!!')
-        console.log( data )
+        console.log( data );
+        // 클릭된 마커가 없고, click 마커가 클릭된 마커가 아니면
+        // 마커의 이미지를 클릭 이미지로 변경
+        if (!this.selectedMarker || this.selectedMarker !== marker1) {
+
+          // 클릭된 마커 객체가 null이 아니면
+          // 클릭된 마커의 이미지를 기본 이미지로 변경하고
+          !!this.selectedMarker && this.selectedMarker.setImage(markerImage);
+
+          // 현재 클릭된 마커의 이미지는 클릭 이미지로 변경
+          marker1.setImage(clickMarkerImage);
+        }
+
+        // 클릭된 마커를 현재 클릭된 마커 객체로 설정
+        this.selectedMarker = marker1;
 
         // 마커를 클릭하면 장소명이 인포윈도우 표출
         this.infowindow.setContent('<div style="padding:5px;font-size:12px;">' + data.place_name + '</div>');
