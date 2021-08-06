55ffcf6d0ef84e4af48bca56a30d89650cb6d3c6
julianne03 <s2019w33@e-mirim.hs.kr>
2021-08-07 01:19:19 +0900

    Add login router in App.ts (#4)

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,17 +1,39 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { User } from "./entity/User";
 
 class App {
   public application: express.Application;
 
   constructor() {
     this.application = express();
+    this.modules();
     this.router();
   }
 
+  private modules(): void {
+    this.application.use(bodyParser.json());
+    this.application.use(bodyParser.urlencoded({ extended: true }));
+  }
+
   private router(): void {
-    this.application.get("/", (req: express.Request, res: express.Response) => {
-      res.send("hello!");
-    });
+    this.application.post(
+      "/api/login",
+      (req: express.Request, res: express.Response) => {
+        User.findOne({ user_email: req.body.userEmail }).then((user) => {
+          if (!user) {
+            const newUser = new User();
+            newUser.user_email = req.body.userEmail;
+            newUser.nickname = "";
+            newUser.image = null;
+            newUser.save();
+          }
+          res.json({
+            loginSuccess: true,
+          });
+        });
+      }
+    );
   }
 }
 
