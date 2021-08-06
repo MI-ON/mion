7ba7640f14fdae9e2ecf66b4c7218ee0f7746bb5
julianne03 <s2019w33@e-mirim.hs.kr>
2021-08-07 01:27:06 +0900

    Check if user is logged in or not when vue app created (#4)

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
+  created() {
+    const userEmail: string | null = localStorage.getItem("userEmail");
+    if (userEmail) {
+      this.$store.commit("SET_USER_EMAIL", userEmail);
+    } else {
+      this.$router.push("/login");
+    }
+  },
   render: (h) => h(App),
 }).$mount("#app");
