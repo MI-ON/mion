2e672709495b35890ca759ca9ab22c358fd5e09b
julianne03 <s2019w33@e-mirim.hs.kr>
2021-08-07 18:00:03 +0900

    Add set,logout user mutations and getters in store (#4)

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -9,8 +9,22 @@ export default new Vuex.Store(<StoreOptions<any>>{
   state: {
     user: {},
   },
-  getters: {},
-  mutations: {},
+  getters: {
+    getUserEmail: (state) => {
+      return state.user.userEmail;
+    },
+    getUserImageUrl: (state) => {
+      return state.user.userImageUrl;
+    },
+  },
+  mutations: {
+    SET_USER(state, user) {
+      state.user = user;
+    },
+    LOGOUT(state) {
+      state.user = {};
+    },
+  },
   actions: {},
   plugins: [
     persistedstate({
