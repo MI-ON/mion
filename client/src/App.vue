7e6a79d4823697c48eeae7c01444ab59b61084d3
mia <s2019w27@e-mirim.hs.kr>
2021-08-09 18:39:55 +0900

    Add icon image and set default SCSS

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,12 +16,5 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-html,
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+@import url('./style.scss');
 </style>
