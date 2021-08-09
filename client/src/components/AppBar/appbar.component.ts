a9ba25cf65b2acc701f36d28fa632b0e557d6464
julianne03 <s2019w33@e-mirim.hs.kr>
2021-08-09 12:03:34 +0900

    Update index.html to add external js file onload cb (#4)

--- client/src/components/AppBar/appbar.component.ts
+++ client/src/components/AppBar/appbar.component.ts
@@ -2,12 +2,6 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class AppBarComponent extends Vue {
-  mounted() {
-    window.gapi.load("auth2", function() {
-      window.gapi.auth2.init();
-    });
-  }
-
   onSignOut(): void {
     window.gapi.auth2.getAuthInstance().disconnect();
     this.$store.commit("LOGOUT");
