6ed9088d7a940851b64055107892f5b413047714
mia <s2019w27@e-mirim.hs.kr>
2021-08-12 23:46:08 +0900

    Update rename onClickRedirect method to onClickRouteRoot (#24)

--- client/src/components/AppBar/appbar.component.ts
+++ client/src/components/AppBar/appbar.component.ts
@@ -1,21 +1,21 @@
-import router from "@/router";
-import { Component, Vue } from "vue-property-decorator";
+import router from '@/router';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class AppBarComponent extends Vue {
-  mounted() {
-    window.gapi.load("auth2", function() {
-      window.gapi.auth2.init();
-    });
-  }
+    mounted() {
+        window.gapi.load('auth2', function() {
+            window.gapi.auth2.init();
+        });
+    }
 
-  onSignOut(): void {
-    window.gapi.auth2.getAuthInstance().disconnect();
-    this.$store.commit("LOGOUT");
-    location.reload();
-  }
+    onSignOut(): void {
+        window.gapi.auth2.getAuthInstance().disconnect();
+        this.$store.commit('LOGOUT');
+        location.reload();
+    }
 
-  onClickRedirect(): void {
-    router.push({ path: "/" });
-  }
+    onClickRouteRoot(): void {
+        router.push({ path: '/' });
+    }
 }
