1f83e2ecf5feff061f69387ea5d0448d0ba29888
mia <s2019w27@e-mirim.hs.kr>
2021-08-14 22:18:54 +0900

    Update member page screen layout task complete (#23)

--- client/src/components/memberPage/member.component.vue
+++ client/src/components/memberPage/member.component.vue
@@ -1,5 +1,23 @@
 <template>
-    <div id="member"></div>
+    <div id="member">
+        <div class="wrapper">
+            <span class="profile-text">프로필 변경</span><br />
+            <img
+                src="../../assets/AppBar/default-profile.png"
+                width="25%"
+                height="25%"
+            /><br />
+            <span class="name">이름</span>
+            <input
+                type="text"
+                class="input-box"
+                v-on:keyup.enter="changeNickname"
+            /><br />
+            <button class="change-nickname" @click="changeNickname">
+                변경
+            </button>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" src="./member.component.ts"></script>
