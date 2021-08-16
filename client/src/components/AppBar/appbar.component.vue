9cd48c89552eaf5d34411136410d21be2bc6638f
mia <s2019w27@e-mirim.hs.kr>
2021-08-16 15:55:05 +0900

    Update add default profile image to top bar, click to move login screen (#24)

--- client/src/components/AppBar/appbar.component.vue
+++ client/src/components/AppBar/appbar.component.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="app-bar">
-    <img
-      class="app-logo"
-      src="../../assets/AppBar/mion_logo.png"
-      width="55"
-      height="55"
-      v-on:click="this.onClickRedirect"
-    />
-    <span class="app-logo-text" v-on:click="this.onClickRedirect">MI:ON</span>
-    <img
-      v-if="this.$store.state.userToken"
-      class="user-profile-image"
-      :src="this.$store.getters.getUserImageUrl"
-      width="52"
-      height="52"
-      v-on:click="this.onSignOut"
-    />
-  </div>
+    <div class="app-bar">
+        <img
+            class="app-logo"
+            src="../../assets/AppBar/mion_logo.png"
+            width="55"
+            height="55"
+            v-on:click="this.onClickRedirect"
+        />
+        <span class="app-logo-text" v-on:click="this.onClickRedirect"
+            >MI:ON</span
+        >
+        <v-avater>
+            <img
+                class="user-profile-image"
+                v-if="this.$store.state.userToken"
+                :src="$store.getters.getUserImageUrl"
+                width="50"
+                height="50"
+                v-on:click="this.onSignOut"
+            />
+            <img
+                v-show="!this.$store.state.userToken"
+                class="user-profile-image"
+                :src="defaultProfile"
+                width="50"
+                height="50"
+                v-on:click="this.onSignOn"
+            />
+        </v-avater>
+    </div>
 </template>
 
 <script lang="ts" src="./appbar.component.ts"></script>
