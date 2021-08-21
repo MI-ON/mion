81024ae14a39295d93726a191ab203ab6cacd4ac
mia <s2019w27@e-mirim.hs.kr>
2021-08-21 22:37:50 +0900

    Update working with the Vote List (#56)

---
+++ client/src/components/mainPage/selectbar/votedplaceitem/votedplaceitem.component.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-layout id="voted-place-item-container">
+      <v-flex id="marker-image-container">
+        <img src="../../../../assets/mainPage/click-marker.png" />
+      </v-flex>
+
+      <v-flex id="voted-place-info-container" colum>
+        <div>{{ votedPlaceData.place_name }}</div>
+        <div>{{ votedPlaceData.address_name }}</div>
+        <div>{{ votedPlaceData.road_address_name }}</div>
+        <span>{{ votedPlaceData.phone }}</span>
+        <a v-bind:href="votedPlaceData.place_url">상세정보 |</a>
+        <a>리뷰보기</a>
+
+        <div id="voted-image-container">
+          <!-- google Img로 수정 -->
+          <img src="../../../../assets/AppBar/default-profile.png" />
+          <img src="../../../../assets/AppBar/default-profile.png" />
+          <img src="../../../../assets/AppBar/default-profile.png" />
+          <img src="../../../../assets/AppBar/default-profile.png" />
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+
+@Component
+export default class PlaceItemComponent extends Vue {
+  @Prop(Object) protected votedPlaceData!: Object;
+  @Watch("votedPlaceData")
+  updateMessage() {
+    console.log("votedplaceitem에서 변경 감지");
+    console.log(this.votedPlaceData);
+  }
+}
+</script>
+<style lang="scss" src="./votedplaceitem.component.scss"></style>
