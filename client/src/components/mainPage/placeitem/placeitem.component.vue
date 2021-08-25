111070e0edd57201b9184e98cd6c61d4893335ac
DAYEON CHOI <dayeon2399@naver.com>
2021-08-26 06:57:16 +0900

    Update search place list item size (#72)

--- client/src/components/mainPage/placeitem/placeitem.component.vue
+++ client/src/components/mainPage/placeitem/placeitem.component.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-container>
-    <v-layout id="placeItemContainer">
-      <v-flex id="markerImageContainer">
-        <img src="../../../assets/mainPage/default-marker.png" />
-      </v-flex>
+  <v-layout id="placeItemContainer">
+    <v-flex id="markerImageContainer">
+      <img src="../../../assets/mainPage/default-marker.png" />
+    </v-flex>
 
-      <v-flex id="placeInfoContainer" colum>
-        <div>{{ searchResult.place_name }}</div>
-        <div>{{ searchResult.address_name }}</div>
-        <div>(지번) {{ searchResult.road_address_name }}</div>
+    <v-flex id="placeInfoContainer" colum>
+      <div>{{ searchResult.place_name }}</div>
+      <div>{{ searchResult.address_name }}</div>
+      <div>(지번) {{ searchResult.road_address_name }}</div>
+      <div>
         <span>{{ searchResult.phone }}</span>
         <a v-bind:href="searchResult.place_url">상세정보</a>
-      </v-flex>
+      </div>
+    </v-flex>
 
-      <v-flex id="voteButtonContainer" colum>
-        <button type="button" v-on:click="this.onClickVoteBtn">
-          <img src="../../../assets/vote-icon.png" />
-        </button>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-flex id="voteButtonContainer" colum>
+      <button type="button" v-on:click="this.onClickVoteBtn">
+        <img src="../../../assets/vote-icon.png" />
+      </button>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts" src="./placeitem.component.ts"></script>
