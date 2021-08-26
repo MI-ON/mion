0d445300e4269ce4bf90509fbce1356e4ae07ac4
suyeon <s2019w27@e-mirim.hs.kr>
2021-08-26 14:15:39 +0900

    Add about files (#74)

---
+++ client/src/components/AboutPage/about.component.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="aboutPageComponent">
+    <img src=""/> <span>이란?</span>
+    <hr>
+    <div id="about-content">
+      미온이란, 미림여자정보과학고등학교에 재학 중이며<br/>
+      온다에 적응하고 있는 귀여운 예비 개발자들이<br/>
+      기획하고 개발한 <span>"온다에서의 한 끼 정하기"</span> 프로그램입니다.
+    </div>
+
+    <span>서비스</span>
+    <hr>
+    <div id="service-box-container">
+      <div id="service-box">
+        <img src=""/>
+        <span>지도</span>
+        <span>ONDA 주변 맛집을 탐색해봐요!</span>
+      </div>
+      <div id="service-box">
+        <img src=""/>
+        <span>투표</span>
+        <span>식당을 투표해 점심을 함께 할 동료를 찾아봐요!</span>
+      </div>
+      <div id="service-box">
+        <img src=""/>
+        <span>리뷰</span>
+        <span>오늘 식사에 대한 온다만의 리뷰를 작성해봐요!</span>
+      </div>
+      <div id="service-box">
+        <img src=""/>
+        <span>발자국</span>
+        <span>식당에 대한 온다만의 식사 로그, 발자국을 남겨봐요!</span>
+      </div>
+    </div>
+    <span>개발자</span>
+    <hr>
+    <div id="developer-list">
+      
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./about.component.ts"></script>
+<style lang="scss" src="./about.component.scss"></style>
