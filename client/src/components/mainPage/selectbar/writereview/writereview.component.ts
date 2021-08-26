a8393505d4c50dd6f37c4e53a99159c69396be2f
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-08-26 10:57:10 +0900

    Update until writereview textarea layout

---
+++ client/src/components/mainPage/selectbar/writereview/writereview.component.ts
@@ -0,0 +1,16 @@
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+@Component({})
+export default class WriteReivewComponent extends Vue{
+    name:string="jackie";
+    title:string='크라이치즈버거 삼성역점';
+    address1:string='서울 강남구 테헤란로 616';
+    address2:string='서울 강남구 대치동 996-17';
+    tel:string='02-566-6244';
+    review_count:number=2;
+    review_rating:string='★★★★★';
+    review_cont:number=3;
+    rating_num:number=4.5;
+    ate_people:number=8-5;
+    review_commend:string="맛있어요~~ 다음에 한번 더 오고 싶어요!맛있어요~~ 다음에 한번 더 오고 싶어요!맛있어요~~ 다음에 한번 더 오고 싶어요!맛있어요~~ 다음에 한번 더 오고 싶어요!"
+}
---
+++ client/src/components/mainPage/selectbar/writereview/writereview.component.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="review-lists" style="margin-left:1.5vw;">
+ <div class="button-div">
+  <button><img src="@/assets/upsize.png" alt="button"></button>
+  </div>
+  <div class="write_list">
+    <div class="store_title">
+      {{this.title}}
+    </div>
+    <div class="store_comment">
+      <h2 class="sub_title">상세 정보</h2>
+      <span>{{this.address1}}</span>
+      <span>{{this.address2}}</span>
+      <span>{{this.tel}}</span>
+    </div>
+   <div class="review_profile">
+     <!--임의로 프로필 이미지를 여러개 깔아둠-->
+      <img
+          class="profile_img"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+        <img
+          class="profile_img"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+        <img
+          class="profile_img"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+        <img
+          class="profile_img"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+        <img
+          class="profile_img"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+        <div class="count_people">+{{this.ate_people}}</div>
+    </div>
+    <div class="write_container">
+    <div class="write_review">
+      <v-rating
+        v-model="rating"
+        background-color="indigo lighten-3"
+        color="indigo"
+        large></v-rating>
+        <textarea
+                type="text"
+                v-model="reviewKeyword"
+                id="review-keyword"
+                size="15"
+                placeholder="후기를 작성해주세요."></textarea>
+      <button><img src="../../../../assets/wirtereview.png" alt="리뷰 작성 버튼"/></button>
+    </div>
+    <div class="store_rating">
+        <span class="store_rating">전체  {{this.review_cont}}  | </span>
+    <v-rating
+      v-model="rating"
+      icon-label="custom icon label text {0} of {1}"></v-rating> {{this.rating_num}}
+    </div>
+    <div class="review_view">
+      <img
+          class="profile_img"
+          style="margin-left: 1vw;"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+          <v-rating
+              v-model="rating"
+              icon-label="custom icon label text {0} of {1}"></v-rating> {{this.rating_num}}
+          <span>{{this.review_commend}}</span>
+          <p>{{this.name}}</p>
+    </div>
+    <div class="review_view">
+      <img
+          class="profile_img"
+          style="margin-left: 1vw;"
+          v-if="this.$store.state.userToken"
+          :src="$store.getters.getUserImageUrl"/>
+         <v-rating
+              v-model="rating"
+              icon-label="custom icon label text {0} of {1}"></v-rating> {{this.rating_num}}
+          <span>{{this.review_commend}}</span>
+          <p>{{this.name}}</p>
+    </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      rating: 4,
+    }),
+  }
+</script>
+<script lang="ts" src="./writereview.component.ts"></script>
+<style lang="scss" src="./writereview.component.scss"></style>
