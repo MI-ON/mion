30301f095e4038734dc442a0224f408cb7d19519
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-08-28 00:00:46 +0900

    Update temporary code

--- client/src/components/AppBar/appbar.component.vue
+++ client/src/components/AppBar/appbar.component.vue
@@ -15,21 +15,31 @@
                 class="user-profile-image"
                 v-if="this.$store.state.userToken"
                 :src="$store.getters.getUserImageUrl"
-                width="52"
-                height="52"/>
+                width="50"
+                height="50"
+            />
             <img
-                    v-show="!this.$store.state.userToken"
-                    class="user-profile-image"
-                    :src="defaultProfile"
-                    width="50"
-                    height="50"/>
-        <div class="menu">
-         <p v-on:click="this.onSignOut">로그아웃</p>
-         <p v-on:click="this.onMemberPage">프로필 수정</p>
-        </div>
-    </v-avater>
+                v-show="!this.$store.state.userToken"
+                class="user-profile-image"
+                :src="defaultProfile"
+                width="50"
+                height="50"
+                v-on:click="this.onSignOn"
+            />
+             <div class="menu">
+            <p v-on:click="this.onSignOut">로그아웃</p>
+            <p v-on:click="this.onMemberPage">프로필 수정</p>
+            </div>
+        </v-avater>
     </div>
 </template>
 
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+// export default {
+//     jQuery('.profile').click(function(){
+//    jQuery(this).toggleClass('active');
+// })
+// }
+// </script>
 <script lang="ts" src="./appbar.component.ts"></script>
 <style lang="scss" src="./appbar.component.scss"></style>
